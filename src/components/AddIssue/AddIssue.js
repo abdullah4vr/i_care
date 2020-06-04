baa07e74989c567777ce3e4fade22d7cd516637d
charizardlm10 <royabhik8991@gmail.com>
2020-06-04 14:21:31 +0530

    Addissue updated

--- src/components/AddIssue/AddIssue.js
+++ src/components/AddIssue/AddIssue.js
@@ -11,42 +11,57 @@ class AddIssue extends Component{
         myLat : 47.44,
         myLong : -122.444,
         up : 0,
-        selectedFile : null
+        selectedfile : null
     }
 
 
-    fileSelectedHandler = event =>{
-        console.log(event.target.files[0]);
-        this.setState({selectedFile : event.target.files[0]});
-        let desc = document.getElementById('desc').value;
-        console.log(desc);
+    fileSelectedHandler=event=>{
+        //console.log(e.target.files[0]);
+        this.setState({
+            selectedfile : event.target.files[0]
+        });
+        //let desc = document.getElementById('desc').value;
+        //console.log(desc);
     }
 
 
-    fileUploadHandler = () =>{
-        const fd = new FormData();
-        let desc = document.getElementById('desc').value;
-        console.log(desc);
-        //fd.set('desc',desc);
-        //fd.append('upfile',this.state.selectedFile,this.state.selectedFile.name);
-        fd.set('title',desc);
-        fd.set('body','foo');
-        fd.set('userId','1');
-        console.log(fd);
-        axios({
-            method: 'post',
-            url: 'https://jsonplaceholder.typicode.com/posts',
-            data: fd,
-            headers: {'content-Type': 'multipart/form-data' }
-            }).then(response =>{
-            console.log(response);
-            document.writeln("Bad");
-            alert("Nothing happened");
-        }).catch(error => {
-            document.writeln(error);
-               alert("Something happened");
-          });
-        alert("What has happened");
+    fileUploadHandler=event=>{
+        console.log(this.state, "The current state");
+        let file = this.state.selectedfile;
+        let imageType = /image.*/;
+
+        if (!file.type.match(imageType))
+        console.log("No image here");
+
+        let fd = new FormData();
+        fd.append('upfile', file);
+        fd.append('desc','Charizard');
+        fd.append('title','Image upload');
+        fd.append('body','foo');
+        fd.append('userId','1');
+
+        for (let key of fd.entries()) {
+            console.log(key[0] + ', ' + key[1]);
+        }
+        // let desc = document.getElementById('desc').value;
+        // console.log(desc);
+        //  //fd.set('desc',desc);
+        //  //fd.append('upfile',this.state.selectedFile,this.state.selectedFile.name);
+        //console.log(fd);
+         axios({
+             method: 'post',
+             url: 'https://mighty-shelf-14586.herokuapp.com/',
+             data: fd,
+             headers: {'content-Type': 'multipart/form-data' }
+             }).then(response =>{
+             console.log(response);
+             //document.writeln("Bad");
+             alert("Something good happened");
+         }).catch(error => {
+             document.writeln(error);
+                alert("Something happened");
+           });
+        // alert("What has happened");
     }
 
     componentDidMount()
@@ -70,9 +85,7 @@ class AddIssue extends Component{
         x.getCurrentPosition(success,failure);
     
 
-		function failure(){
-			console.log("Error occured");
-        }
+		 
     }
     render()
     {  
@@ -97,15 +110,14 @@ class AddIssue extends Component{
                           id="custom-file"
                           label="Upload the image"
                           name="upfile"
-                          onChange = {this.fileSelectedHandler}
-                               custom
-                              />
+                          onChange ={this.fileSelectedHandler}
+                          custom />
                     </Form.Group >
                    <Form.Group  style = {{'marginTop' : '10%'}}>
                    <Form.Label><strong>Issue Description</strong></Form.Label>
                    <Form.Control name='desc' type="text" placeholder="Briefly describe the issue " id="desc"/>
                     </Form.Group>
-                    <Button type="submit" onClick={this.fileUploadHandler}>Submit form</Button>
+                    <Button type="button" onClick={this.fileUploadHandler} >Submit form</Button>
                         </Form>  
                         </Container>                            
                     </Container> 
